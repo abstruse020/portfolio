5aa9a55af2c29ae70dbb05138c3d67cbd7392c38
harsh <bokharsh@gmail.com>
2020-03-21 01:17:18 +0530

    Updates

---
+++ precache-manifest.0afd38afaadd5ecb04177a379f6b001e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9536b957a752e2f084e3d1338a14b1c",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "7d64622420c71615e318",
+    "url": "/portfolio/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "f647a73dfce42e9e841b",
+    "url": "/portfolio/static/js/2.5b70e15d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.5b70e15d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d64622420c71615e318",
+    "url": "/portfolio/static/js/main.299003de.chunk.js"
+  },
+  {
+    "revision": "9001bc976e1e9cf1fd4d",
+    "url": "/portfolio/static/js/runtime-main.cf8bc7fe.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
