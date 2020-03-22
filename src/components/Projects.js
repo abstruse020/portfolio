3793ace74e6261025c05e77aa9811951ac922cca
harsh <bokharsh@gmail.com>
2020-03-22 22:35:34 +0530

    skills images done

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -10,17 +10,45 @@ class Projects extends Component {
 
   }
 
+  giveImage(var1){
+    if (var1=='../Assets/CppLogo.png') {
+      return(require('../Assets/CppLogo.png'));
+    }
+    else if(var1=='../Assets/PythonLogo.png'){
+      return(require('../Assets/PythonLogo.png'));
+    }
+    else if(var1=='../Assets/AndroidLogo.png'){
+      return(require('../Assets/AndroidLogo.png'));
+    }
+    else if(var1=='../Assets/PhpLogo.png'){
+      return(require('../Assets/PhpLogo.png'));
+    }
+    else if(var1=='../Assets/FlaskLogo.png'){
+      return(require('../Assets/FlaskLogo.png'));
+    }
+    else if(var1=='../Assets/ReactLogo.png'){
+      return(require('../Assets/ReactLogo.png'));
+    }
+    else{
+      return(require('../Assets/Default.png'));
+    }
+  }
+
   render(){
     return(
       <div className='project_cover'>
-        <div>
+        <heading className='project_heading'>P<br/>R<br/>O<br/>J<br/>E<br/>C<br/>T</heading>
+        <div className='card-columns'>
           {
             this.projectData.map((data, index) => (
               <dvi className=''>
-                <div key={index} className="card skills_card align-items-center">
-                  <img className='card-img-top skills_img mx-auto d-block' src={cpplogo} alt='no img'></img>
+                <div key={index} className="card projects_card align-items-center">
+                  <img className='card-img-top proj_img mx-auto d-block' src={this.giveImage(data.link)} alt='no img'></img>
                   <p className="card-body text-center">
                     <span className="card-title">{data.name}</span>
+                  </p>
+                  <p className='card-text'>
+                    {data.about}
                   </p>
                 </div>
               </dvi>
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -9,7 +9,34 @@ class Skills extends Component {
     super(props);
 
     this.skillsData = props.SkillsData;
+    this.state = {
+      "CpplLogo": require('../Assets/CppLogo.png'),
+      "PythonLogo": require('../Assets/CppLogo.png')
+    }
 
+  }
+  giveImage(var1){
+    if (var1=='../Assets/CppLogo.png') {
+      return(require('../Assets/CppLogo.png'));
+    }
+    else if(var1=='../Assets/PythonLogo.png'){
+      return(require('../Assets/PythonLogo.png'));
+    }
+    else if(var1=='../Assets/AndroidLogo.png'){
+      return(require('../Assets/AndroidLogo.png'));
+    }
+    else if(var1=='../Assets/PhpLogo.png'){
+      return(require('../Assets/PhpLogo.png'));
+    }
+    else if(var1=='../Assets/FlaskLogo.png'){
+      return(require('../Assets/FlaskLogo.png'));
+    }
+    else if(var1=='../Assets/ReactLogo.png'){
+      return(require('../Assets/ReactLogo.png'));
+    }
+    else{
+      return(require('../Assets/Default.png'));
+    }
   }
   render(){
     return(
@@ -18,14 +45,12 @@ class Skills extends Component {
           <div className='card-columns'>
             {
               this.skillsData.map((data, index) => (
-                <dvi className=''>
                   <div key={index} className="card skills_card align-items-center">
-                    <img className='card-img-top skills_img mx-auto d-block' src={cpplogo} alt='no img'></img>
+                    <img className='card-img-top skills_img mx-auto d-block' src={this.giveImage(data.link)} alt='no img'></img>
                     <p className="card-body text-center">
                       <span className="card-title">{data.name}</span>
                     </p>
                   </div>
-                </dvi>
               ))
             }
           </div>
