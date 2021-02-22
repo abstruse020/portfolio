fb89a6e96fe2059d864c33cfad73953535e87e9b
harsh <bokharsh@gmail.com>
2021-02-22 21:46:57 +0530

    Updates

---
+++ precache-manifest.8caf6b3ca41a6dba19e125ddac8c6bc2.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e2c93eafdc3c969f13113c9006ab125",
+    "url": "/index.html"
+  },
+  {
+    "revision": "defb2e2a91683cf9a9b6",
+    "url": "/static/css/main.98de97e4.chunk.css"
+  },
+  {
+    "revision": "c01dffcf0f01fc9bdd21",
+    "url": "/static/js/2.306cade2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.306cade2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "defb2e2a91683cf9a9b6",
+    "url": "/static/js/main.7cb2daf3.chunk.js"
+  },
+  {
+    "revision": "00e21f668a1b72c19744",
+    "url": "/static/js/runtime-main.b4529488.js"
+  },
+  {
+    "revision": "2db2fad9419e15577da12aa316179abb",
+    "url": "/static/media/AndroidLogo.2db2fad9.png"
+  },
+  {
+    "revision": "8f42e018a9340b6e4e20161b06525308",
+    "url": "/static/media/CppLogo.8f42e018.png"
+  },
+  {
+    "revision": "d8f3825c0ac1f3df4e430967a116bfac",
+    "url": "/static/media/FlaskLogo.d8f3825c.png"
+  },
+  {
+    "revision": "2c333c67eea4fb05574bce212f70a727",
+    "url": "/static/media/JavaLogo.2c333c67.png"
+  },
+  {
+    "revision": "b9ed1bea23660b6da28427c33adebe9d",
+    "url": "/static/media/PhpLogo.b9ed1bea.png"
+  },
+  {
+    "revision": "31f6b12d5f5ac94d19487edfd535729c",
+    "url": "/static/media/PythonLogo.31f6b12d.png"
+  },
+  {
+    "revision": "b0d73164c873fc07e567c0f2c6a7879c",
+    "url": "/static/media/digitRecognizer.b0d73164.png"
+  },
+  {
+    "revision": "494f673ff11b5546e81d4757be629602",
+    "url": "/static/media/profile.494f673f.jpg"
+  },
+  {
+    "revision": "5be78b15bd1d3444f067ec78fa353fb8",
+    "url": "/static/media/resume.5be78b15.pdf"
+  },
+  {
+    "revision": "639b9698ffe41b1ee9a751be7b00fb4e",
+    "url": "/static/media/sRS.639b9698.png"
+  }
+]);
