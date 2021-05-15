c2858a08d05de68671668ed99b089669fb52377d
harsh <bokharsh@gmail.com>
2021-05-15 18:19:00 +0530

    Updates

---
+++ precache-manifest.192e7823450efc9b2dfb19e4e08b13b3.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "803acf478880e294b602a011b088ac65",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "ae2e72f198a67040f9d2",
+    "url": "/portfolio/static/css/main.98de97e4.chunk.css"
+  },
+  {
+    "revision": "c01dffcf0f01fc9bdd21",
+    "url": "/portfolio/static/js/2.306cade2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.306cade2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae2e72f198a67040f9d2",
+    "url": "/portfolio/static/js/main.fceba909.chunk.js"
+  },
+  {
+    "revision": "9001bc976e1e9cf1fd4d",
+    "url": "/portfolio/static/js/runtime-main.cf8bc7fe.js"
+  },
+  {
+    "revision": "2db2fad9419e15577da12aa316179abb",
+    "url": "/portfolio/static/media/AndroidLogo.2db2fad9.png"
+  },
+  {
+    "revision": "8f42e018a9340b6e4e20161b06525308",
+    "url": "/portfolio/static/media/CppLogo.8f42e018.png"
+  },
+  {
+    "revision": "d8f3825c0ac1f3df4e430967a116bfac",
+    "url": "/portfolio/static/media/FlaskLogo.d8f3825c.png"
+  },
+  {
+    "revision": "2c333c67eea4fb05574bce212f70a727",
+    "url": "/portfolio/static/media/JavaLogo.2c333c67.png"
+  },
+  {
+    "revision": "b9ed1bea23660b6da28427c33adebe9d",
+    "url": "/portfolio/static/media/PhpLogo.b9ed1bea.png"
+  },
+  {
+    "revision": "31f6b12d5f5ac94d19487edfd535729c",
+    "url": "/portfolio/static/media/PythonLogo.31f6b12d.png"
+  },
+  {
+    "revision": "b0d73164c873fc07e567c0f2c6a7879c",
+    "url": "/portfolio/static/media/digitRecognizer.b0d73164.png"
+  },
+  {
+    "revision": "494f673ff11b5546e81d4757be629602",
+    "url": "/portfolio/static/media/profile.494f673f.jpg"
+  },
+  {
+    "revision": "5be78b15bd1d3444f067ec78fa353fb8",
+    "url": "/portfolio/static/media/resume.5be78b15.pdf"
+  },
+  {
+    "revision": "639b9698ffe41b1ee9a751be7b00fb4e",
+    "url": "/portfolio/static/media/sRS.639b9698.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8caf6b3ca41a6dba19e125ddac8c6bc2.js"
+  "/portfolio/precache-manifest.192e7823450efc9b2dfb19e4e08b13b3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
